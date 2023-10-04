95918263da7189966749eca40cb75c9dcca39a33
SanduniIshara <sanduniishara1@outlook.com>
2023-10-05 00:17:15 +0530

    add timekeeper homepage

--- src/main/java/com/smoothtix/database/dbConnection.java
+++ src/main/java/com/smoothtix/database/dbConnection.java
@@ -10,7 +10,7 @@ public class dbConnection {
         String dbURL = "jdbc:mysql:// localhost:3306/";
         String dbName = "SmoothTix";
         String dbUsername = "root";
-        String dbPassword = "";
+        String dbPassword = "W@2915djkq#";
         Class.forName(dbDriver);
         Connection con = DriverManager.getConnection(dbURL + dbName, dbUsername, dbPassword);
         return con;
