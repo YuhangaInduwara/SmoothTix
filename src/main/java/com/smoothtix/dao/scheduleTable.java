52b70c180a49c696f0705ee16f304e576934e2e0
SanduniIshara <sanduniishara1@outlook.com>
2023-10-24 06:39:09 +0530

    create js file

--- src/main/java/com/smoothtix/dao/scheduleTable.java
+++ src/main/java/com/smoothtix/dao/scheduleTable.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 public class scheduleTable {
     public static int insert(Schedule schedule) throws SQLException, ClassNotFoundException {
         Connection con = dbConnection.initializeDatabase();
-        PreparedStatement pst = con.prepareStatement("insert into bus(bus_id, owner_id, engineNo, chassisNo, noOfSeats, manufact_year, brand, model) values (?,?,?,?,?,?,?,?)");
+        PreparedStatement pst = con.prepareStatement("insert into shedule(schedule_id, date, route_id, start, destination, start_time, end_time) values (?,?,?,?,?,?,?)");
         pst.setString(1,schedule.getSchedule_id());
         pst.setString(2,schedule.getDate());
         pst.setString(3,schedule.getRoute_id());
@@ -26,10 +26,10 @@ public class scheduleTable {
         return rawCount;
     }
 
-    public static ResultSet get(String bus_id) throws SQLException, ClassNotFoundException {
+    public static ResultSet get(String schedule_id) throws SQLException, ClassNotFoundException {
         Connection con = dbConnection.initializeDatabase();
-        PreparedStatement pst = con.prepareStatement("SELECT * FROM bus WHERE bus_id=?");
-        pst.setString(1,bus_id);
+        PreparedStatement pst = con.prepareStatement("SELECT * FROM schedule WHERE schedule_id=?");
+        pst.setString(1,schedule_id);
         ResultSet rs = pst.executeQuery();
 //        con.close();
         return rs;
@@ -43,27 +43,26 @@ public class scheduleTable {
         return rs;
     }
 
-    public static int update(String bus_id, Bus bus) throws SQLException, ClassNotFoundException {
+    public static int update(String schedule_id, Schedule schedule) throws SQLException, ClassNotFoundException {
         Connection con = dbConnection.initializeDatabase();
-        PreparedStatement pst = con.prepareStatement("UPDATE bus SET bus_id=?, owner_id=?, engineNo=?, chassisNo=?, noOfSeats=?, manufact_year=?, brand=?, model=? WHERE bus_id=?");
-        pst.setString(1,bus.getBus_id());
-        pst.setString(2,bus.getOwner_id());
-        pst.setString(3,bus.getEngineNo());
-        pst.setString(4,bus.getChassisNo());
-        pst.setInt(5,bus.getNoOfSeats());
-        pst.setString(6,bus.getManufact_year());
-        pst.setString(7,bus.getBrand());
-        pst.setString(8,bus.getModel());
-        pst.setString(9,bus_id);
+        PreparedStatement pst = con.prepareStatement("UPDATE bus SET schedule_id=?, date=?, route_id=?, start=?, destination=?, start_time=?, end_time=? WHERE schedule_id=?");
+        pst.setString(1,schedule.getSchedule_id());
+        pst.setString(2,schedule.getDate());
+        pst.setString(3,schedule.getRoute_id());
+        pst.setString(4,schedule.getStart());
+        pst.setString(5,schedule.getDestination());
+        pst.setString(6,schedule.getStart_time());
+        pst.setString(7,schedule.getEnd_time());
+        pst.setString(9,schedule_id);
         int rawCount = pst.executeUpdate();
         con.close();
         return rawCount;
     }
 
-    public static int delete(String bus_id) throws SQLException, ClassNotFoundException {
+    public static int delete(String schedule_id) throws SQLException, ClassNotFoundException {
         Connection con = dbConnection.initializeDatabase();
-        PreparedStatement pst = con.prepareStatement("DELETE FROM bus WHERE bus_id = ?");
-        pst.setString(1,bus_id);
+        PreparedStatement pst = con.prepareStatement("DELETE FROM schedule WHERE schedule_id = ?");
+        pst.setString(1,schedule_id);
         int rawCount = pst.executeUpdate();
         con.close();
         return rawCount;
