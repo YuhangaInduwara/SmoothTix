d1a9d93205581d256e0a6d5be55ad8ad7ae0fe95
InodShanilka <shanilkainod@gmail.com>
2024-02-04 08:19:00 +0530

    added review controller java file

--- src/main/java/com/smoothtix/dao/reviewTable.java
+++ src/main/java/com/smoothtix/dao/reviewTable.java
@@ -5,23 +5,32 @@ import com.smoothtix.model.Review;
 
 import java.sql.*;
 
-//public class reviewTable {
-//    public static String insert(Review review) throws SQLException, ClassNotFoundException {
-//        Connection con = dbConnection.initializeDatabase();
-//        PreparedStatement pst = con.prepareStatement("insert into review(review_id, point_id, bus_profile_id, comment) values (?,?,?,?)");
+public class reviewTable {
+    public static String insert(Review review) throws SQLException, ClassNotFoundException {
+        Connection con = dbConnection.initializeDatabase();
+        PreparedStatement pst = con.prepareStatement("insert into review(review_id, point_id, bus_profile_id, comment) values (?,?,?,?)");
 //        String review_id = generateReviewID();
-//        pst.setString(1, payment_id);
-//        pst.setString(2, payment.getDate_Time());
-//        pst.setDouble(3, payment.getAmount());
-//
-//        int rawCount = pst.executeUpdate();
-//        if(rawCount >= 0){
+//        String point_id = generatePointID();
+//        String busProfile_id = getBusProfileID();
+//        pst.setString(1, review_id);
+//        pst.setString(2, point_id);
+//        pst.setString(3, busProfile_id);
+        pst.setString(4, review.getComments());
+
+        int rawCount = pst.executeUpdate();
+        if(rawCount >= 0){
 ////            return payment_id;
 //            return "{\"payment_id\":\"" + payment_id + "\"}";
-//        }
-//        return "Unsuccessful";
+        }
+        return "Unsuccessful";
+    }
+
+//    public static String generateReviewID() throws SQLException{
+//
+//
+//        return "RV" + bookingID.subString(2);
 //    }
-//
-//
-//
-//}
+
+
+
+}
